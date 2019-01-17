2908229291eeb7b236b79baaf9ee89b7370172f3
Weeaboo420 <43264625+Weeaboo420@users.noreply.github.com>
2019-01-17 23:41:53 +0100

    Fixed recursion problem.
    I fixed a problem with Python's recursion handling. So basically, in the first version you could not generate numbers greater than 500 twice or else you would get an error. This is now fixed and you can generate a code with a length of at least 100k characters.

--- alphagen.py
+++ alphagen.py
@@ -1,7 +1,23 @@
 import os, sys, random, time
 
-os.system("cls && color 0B")
+
+#terminal color
+colors = ["A", "B", "C", "D", "E", "F"]
+randomColorNum = random.choice(colors)
+myColor = "0" + randomColorNum
+
+
+os.system("cls && color " + myColor)
 ver = "1.0"
+
+#universal vars
+isGenerating = False
+
+index = 1
+myKey = ""
+mySize = 0
+
+
 
 
 def start():
@@ -11,7 +27,11 @@ def start():
     size = input("Code length? ")
 
     if size.isdigit():
-            generate(int(size))
+            isGenerating = True
+            #print(isGenerating)
+
+            for i in range((int(size) + 1)):
+                generate(int(size), isGenerating)
 
     else:
         print("")
@@ -20,69 +40,82 @@ def start():
         start()
 
 
-#universal vars
-index = 1
-myKey = ""
 
-def generate(sz = 10):
+def generate(sz = 10, gen = True):
 
-
-    #global vars
     global index
     global myKey
-    
-    mySize = sz
+    global mySize
 
-    
-    alpha_c = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    alpha_l = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-    
-    colors = ["9", "A", "B", "C", "D", "E", "F"]
-    randomColorNum = random.choice(colors)
-    myColor = "0" + randomColorNum
-    
+    #print("stage 1")
 
-    r_select = random.randint(1,3)
-    r_char_alpha = random.randint(1, 26)
-    r_char_num = random.randint(1, 10)
-
-
-    if index < (sz + 1):
-        if r_select == 1:
-            #Capital letters
-            myChar = random.choice(alpha_c)
-
-        if r_select == 2:
-            #Lowercase letters
-            myChar = random.choice(alpha_l)
-            
-        if r_select == 3:
-            #Numbers
-            myChar = random.choice(num)
-
-
-        index += 1
-        myKey = myKey + myChar
-        generate(mySize)
-
-            
-    else:
-        print("")
-        print(myKey)
-        print("")
-        print("")
-
-
-        #nullify obsolete variables
-        index = 1
-        myKey = ""
-        mySize = 0
-
-        os.system("color " + myColor)
-        myColor = ""
+    while gen:
         
-        start()
+        #print("stage 2")
+
+        r_select = 0
+        r_char_alpha = 0
+        r_char_num = 0
+
+        mySize = sz
+        
+        alpha_c = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        alpha_l = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+        
+
+        
+
+        r_select = random.randint(1,3)
+        r_char_alpha = random.randint(1, 26)
+        r_char_num = random.randint(1, 10)
+
+    
+        while (index < (sz + 1)):
+        
+            if r_select == 1:
+                #Capital letters
+                myChar = random.choice(alpha_c)
+
+
+            if r_select == 2:
+                #Lowercase letters
+                myChar = random.choice(alpha_l)
+
+            if r_select == 3:
+                #Numbers
+                myChar = random.choice(num)
+         
+
+            index += 1
+            myKey = myKey + myChar
+            
+            return r_select
+            return r_char_alpha
+            return r_char_num
+            
+            #nullify obsolete variables
+            print("Here")
+
+                
+        while (index == (sz + 1) and gen == True):
+            print("")
+            print(myKey)
+            print("")
+            print("")
+
+
+            #nullify obsolete variables
+            index = 1
+            myKey = ""
+            mySize = 0
+            r_select = None
+            r_char_alpha = None
+            r_char_num = None
+            
+
+            gen = False
+            start()
 
 
 
