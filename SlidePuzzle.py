6f1281a55b32a370d60c7b9a5eb1df2eaa7e383e
Weeaboo420 <43264625+Weeaboo420@users.noreply.github.com>
2019-04-04 18:57:37 +0200

    Made every variation solvable
    I made it so that the starting position is the solved position and then just have the program scramble it to make it more random but the important thing is that it will always be solvable because the starting position is the solved position.

--- SlidePuzzle.py
+++ SlidePuzzle.py
@@ -28,45 +28,17 @@ def generate():
     row2 = ["4", "5", "6"]
     row3 = ["7", "8", "*"]
 
-    c1 = row1[random.randint(0, len(row1)-1)]
-    s1 = symbol(1, 1, fix(c1))
-    row1.remove(c1)
+    s1 = symbol(1, 1, fix(row1[0]))
+    s2 = symbol(2, 1, fix(row1[1]))
+    s3 = symbol(3, 1, fix(row1[2]))
 
-    c2 = row1[random.randint(0, len(row1)-1)]
-    s2 = symbol(2, 1, fix(c2))
-    row1.remove(c2)
+    s4 = symbol(1, 2, fix(row2[0]))
+    s5 = symbol(2, 2, fix(row2[1]))
+    s6 = symbol(3, 2, fix(row2[2]))
 
-    c3 = row1[random.randint(0, len(row1)-1)]
-    s3 = symbol(3, 1, fix(c3))
-    row1.remove(c3)
-
-
-
-    c4 = row2[random.randint(0, len(row2)-1)]
-    s4 = symbol(1, 2, fix(c4))
-    row2.remove(c4)
-
-    c5 = row2[random.randint(0, len(row2)-1)]
-    s5 = symbol(2, 2, fix(c5))
-    row2.remove(c5)
-
-    c6 = row2[random.randint(0, len(row2)-1)]
-    s6 = symbol(3, 2, fix(c6))
-    row2.remove(c6)
-
-
-
-    c7 = row3[random.randint(0, len(row3)-1)]
-    s7 = symbol(1, 3, fix(c7))
-    row3.remove(c7)
-
-    c8 = row3[random.randint(0, len(row3)-1)]
-    s8 = symbol(2, 3, fix(c8))
-    row3.remove(c8)
-
-    c9 = row3[random.randint(0, len(row3)-1)]
-    s9 = symbol(3, 3, fix(c9))
-    row3.remove(c9)
+    s7 = symbol(1, 3, fix(row3[0]))
+    s8 = symbol(2, 3, fix(row3[1]))
+    s9 = symbol(3, 3, fix(row3[2]))
 
 
     symbols.append(s1)
@@ -87,6 +59,37 @@ def cleanup():
     symbols.clear()
     
 
+
+def scramble():
+    s = True
+
+    while s:
+        for x in range(30, random.randint(30, 100)):
+            r = random.randint(1, 6)
+
+            if r == 1 or r == 2:
+                down()
+                left()
+                left()
+                up()
+            elif r == 3:
+                up()
+                right()
+                down()
+            elif r == 4 or r == 5:
+                left()
+                left()
+                up()
+            elif r == 6:
+                right()
+                down()
+                down()
+                left()
+
+        if symbols[0].val == "1" and symbols[1].val == "2" and symbols[2].val == "3" and symbols[3].val == "4" and symbols[4].val == "5":
+            pass
+        else:
+            s = False
 
 
 def p(element, element2, element3):
@@ -140,8 +143,6 @@ def up():
 
         myBlock.val = newBlock.val
         newBlock.val = "*"
-        clear()
-        draw()
 
     else:
         pass
@@ -157,8 +158,6 @@ def down():
 
         myBlock.val = newBlock.val
         newBlock.val = "*"
-        clear()
-        draw()
 
     else:
         pass
@@ -174,8 +173,6 @@ def left():
 
         myBlock.val = newBlock.val
         newBlock.val = "*"
-        clear()
-        draw()
 
     else:
         pass
@@ -191,8 +188,6 @@ def right():
 
         myBlock.val = newBlock.val
         newBlock.val = "*"
-        clear()
-        draw()
 
     else:
         pass
@@ -200,6 +195,8 @@ def right():
 
 
 generate()
+scramble()
+clear()
 draw()
 run = True
 while run:
@@ -208,20 +205,29 @@ while run:
         pass
     elif c == "up" or c == "w":
         up()
+        clear()
+        draw()
         
     elif c == "down" or c == "s":
         down()
+        clear()
+        draw()
 
     elif c == "left" or c == "a":
         left()
+        clear()
+        draw()
 
     elif c == "right" or c == "d":
         right()
+        clear()
+        draw()
         
     elif c == "new" or c == "reload":
         cleanup()
         clear()
         generate()
+        scramble()
         draw()
 
     else:
