95abcf6d46ae87522a26ac70caaa153a163632b6
Richard Gustafsson <guest.richard.gustafsson@hogia.se>
2020-02-27 12:51:59 +0100

    (guid.py): Added output file functionality

--- guid.py
+++ guid.py
@@ -1,4 +1,4 @@
-import random, sys
+import random, sys, os
 
 letters = ["a", "b", "c", "d", "e", "f"]
 numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
@@ -14,7 +14,7 @@ def spit(n):
 	outputString = ""
 	for temp in range(n+1):
 		randomNumber = random.randint(0, 1)
-		
+
 		#letter
 		if randomNumber == 0:
 			if(capital == False):
@@ -34,31 +34,63 @@ def spit(n):
 args = sys.argv
 print("")
 
+#a variable to store the output filename if it is specified
+myFileName = ""
+
+
 if len(args) > 1:
 
 	#check for the arg that specifies uppercase or not
-	if len(args) >= 3:
-		try:
-			if args[2].lower() == "true":
-				capital = True
-			else:
-				pass
-		except:
+	try:
+		if not os.path.isfile(args[3]):
+			myFileName = args[3]
+			with open(myFileName, mode="x+") as f:
+				f.close()
+			#create a file with the specified name
+		else:
+			print("[ERROR] Invalid file path or the file already exists")
+			print("")
+
+	except:
+		pass
+
+
+
+	try:
+		if args[2].lower() == "true":
+			capital = True
+		else:
 			pass
+	except:
+		pass
 
 
 	try:
 		for temp in range (int(args[1])):
-			print(f"{spit(8)}-{spit(4)}-{spit(4)}-{spit(4)}-{spit(12)}")
+			printedLine = f"{spit(8)}-{spit(4)}-{spit(4)}-{spit(4)}-{spit(12)}"
+			print(printedLine)
+
+			if os.path.isfile(myFileName) and myFileName != "":
+				with open(myFileName, "a+") as fh:
+					fh.write(printedLine + "\n")
+					fh.close()
+
 	except:
 		print(f"{spit(8)}-{spit(4)}-{spit(4)}-{spit(4)}-{spit(12)}")
 
 else:
-	print("Usage: guid [n] [useCaps]]")
+	print("Usage: guid [n] [useCaps] [outputFile]")
+	print("")
 	print("[n | 1 -> infinity] is the amount of guids you wish to create")
 	print("[useCaps | true, false] is a bool that forces all letters to be in caps")
+	print("[outputFile] is the path to the output file that will be created (*a)")
+	print("")
+
 	print("")
 	print("Examples:")
 	print("guid 10 false --- return 10 guids in lowercase")
 	print("guid 3 true --- return 3 guids with uppercase")
 
+	print("")
+	print("Legends:")
+	print("*a = This program will never overwrite or append to existing files, only create new ones.")
