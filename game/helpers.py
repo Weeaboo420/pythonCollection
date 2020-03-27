51a022e0e128d47fd4c60b490873fcdeeaae690f
Richard Gustafsson <guest.richard.gustafsson@hogia.se>
2020-03-27 14:34:05 +0100

    refactor(helpers.py): Optimized the function that displays the player's health

--- game/helpers.py
+++ game/helpers.py
@@ -6,43 +6,31 @@ def clear():
 
 def getHealth(player):
     if type(player) is classes.Player:
-        health = ""
+        
+        hitPoints = 0
+
+        for ticks in range(0, player.currentHp, 10):
+            if ticks % 10 == 0:
+                hitPoints += 1        
+
+        healthString = ""
         suffix = ""
 
-        if player.currentHp == 100:
-            health = "[==========]" + suffix
-        
-        if player.currentHp == 90:
-            health = "[========= ]" + suffix
+        if player.currentHp > 0:
+            healthString = "["
+            for hp in range(0, hitPoints, 1):
+                healthString += "="
+            
+            if hitPoints < 10:
+                for missingHp in range(0, 10 - hitPoints, 1):
+                    healthString += " "
 
-        if player.currentHp == 80:
-            health = "[========  ]" + suffix
+            healthString += "]"
+            #health = "[==========]" + suffix
+        else:
+            healthString = "[   DEAD   ]" + suffix
 
-        if player.currentHp == 70:
-            health = "[=======   ]" + suffix
-
-        if player.currentHp == 60:
-            health = "[======    ]" + suffix
-
-        if player.currentHp == 50:
-            health = "[=====     ]" + suffix
-
-        if player.currentHp == 40:
-            health = "[====      ]" + suffix
-
-        if player.currentHp == 30:
-            health = "[===       ]" + suffix
-        
-        if player.currentHp == 20:
-            health = "[==        ]" + suffix
-
-        if player.currentHp == 10:
-            health = "[=         ]" + suffix
-
-        if player.currentHp == 0:
-            health = "[   DEAD   ]" + suffix
-
-        return classes.Colors.OKGREEN + health + classes.Colors.ENDC
+        return classes.Colors.OKGREEN + healthString + classes.Colors.ENDC
     else:
         raise TypeError("ERROR: Argument \'player\' has to be of type \'Player\'!")
 
@@ -53,7 +41,7 @@ def displayLegend(room):
         print(f"{classes.Colors.OKGREEN}P = Player{classes.Colors.ENDC}, {classes.Colors.OKBLUE}N = NPC{classes.Colors.ENDC}")
         print("[w, a, s, d] + [RETURN] to move")
         print("")
-        print(f"Your health: {getHealth(room.player)}")
+        print(f"HP: {getHealth(room.player)} ({room.player.currentHp})")
         print("")
     else:
 	    raise TypeError("ERROR: argument \'room\' must be of type Room!")
