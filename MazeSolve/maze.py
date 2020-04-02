db21d468cde0594b2159a2bb15d5b0b26b8eb122
Richard Gustafsson <guest.richard.gustafsson@hogia.se>
2020-04-02 17:04:52 +0200

    feat(maze.py): Added dark mode

--- MazeSolve/maze.py
+++ MazeSolve/maze.py
@@ -1,5 +1,7 @@
-import PIL, os
+import PIL, os, sys
 from PIL import Image
+
+ 
 
 class pixel():
     def __init__(self, x, y, rgba):
@@ -10,15 +12,23 @@ class pixel():
         if self.rgba == (255, 255, 255, 255) or self.rgba == (255, 255, 255):
             self.white = True
 
-#vars
-            
-myColor = (66, 244, 173, 255)
-dead_end = (244, 46, 46, 255)
+#vars   
+green = (107, 255, 179, 255)
+red = (255, 79, 79, 255)
 blue = (66, 135, 245, 255)
-gold = (252, 173, 3, 255)
+purple = (163, 4, 207, 255)
+special = (85, 84, 125, 255)
 
-pixels, dead_ends, junctions = [], [], []
+dark_mode = False
+
+pixels, reds, junctions = [], [], []
 start, goal = pixel(0, 0, (0,0,0,0)), pixel(0, 0, (0,0,0,0))
+argv = sys.argv
+
+if len(argv) == 2:
+    if argv[1].lower() == "-dark" or argv[1].lower() == "-darkmode" or argv[1].lower() == "-dark_mode":
+        dark_mode = True
+       
 
 def find_pixel(x, y):
     for p in pixels:
@@ -81,49 +91,55 @@ def find_junctions(width, height):
                 junctions.append(a)
 
             elif v == 1 and h == 0 and a.y != (height-1):
-                dead_ends.append(a)                
+                reds.append(a)                
 
             elif h == 1 and v == 0:
-                dead_ends.append(a)
+                reds.append(a)
         else:
             pass        
 
 def scan():
     maze = Image.open("maze.png")
-
     width, height = maze.width, maze.height    
     
     if os.path.isfile("maze_enlarged.png"):
-        pass
-    else:
-        maze_enlarged = maze.resize((width*100, height*100), PIL.Image.NEAREST)
-        maze_enlarged.save("maze_enlarged.png")
+        os.remove("maze_enlarged.png")    
+
+    maze_enlarged = maze.resize((width*100, height*100), PIL.Image.NEAREST)
+    maze_enlarged.save("maze_enlarged.png")
 
     print(f"Scanning maze.png with a size of ({width}, {height})")
     output = Image.new("RGB", (width, height), 0)
-
+    if dark_mode:
+        print("[dark mode enabled]")
 
     #scan and add all pixels to a list
     for y in range(0, height):
         for x in range(0, width):
             myPixel = pixel(x, y, maze.getpixel((x,y)))
             pixels.append(myPixel)
-            output.putpixel((x, y), maze.getpixel((x, y)))
+            if not dark_mode:
+                output.putpixel((x, y), maze.getpixel((x, y)))
+            else:
+                darkerColor = maze.getpixel((x, y))
+                if darkerColor[0] == 255 and darkerColor[1] == 255 and darkerColor[2] == 255:
+                    darkerColor = special
+                output.putpixel((x, y), darkerColor)
 
     find_junctions(width, height)            
     junction_count = 0
     #compare pixels to junction pixels
     for j in junctions:
-        output.putpixel((j.x, j.y), myColor)
+        output.putpixel((j.x, j.y), green)
         junction_count += 1
 
     dead_count = 0
-    for d in dead_ends:
-        output.putpixel((d.x, d.y), dead_end)
+    for d in reds:
+        output.putpixel((d.x, d.y), red)
         dead_count += 1
 
     output.putpixel((start.x, start.y), blue)
-    output.putpixel((goal.x, goal.y), gold)
+    output.putpixel((goal.x, goal.y), purple)
 
     print(f"Pixels: {len(pixels)}, Junction pixels: {junction_count}, Dead ends: {dead_count}")
     choice = input("Do you want to save the solution? (y/n) ")
