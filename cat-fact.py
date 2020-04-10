14ab32a3e3d97c42fc3657a63a0f5f0cb415a72b
Weeaboo420 <43264625+Weeaboo420@users.noreply.github.com>
2020-04-10 10:20:20 +0200

    feat(cat-fact.py): Added author and upvote information, added script author information, added padding

--- cat-fact.py
+++ cat-fact.py
@@ -4,6 +4,20 @@ url = "https://cat-fact.herokuapp.com/facts"
 response = urlopen(url)
 data_raw = json.loads(response.read())
 
-data = data_raw["all"][random.randrange(0, len(data_raw["all"]))]
-print(data.get("text"))
-
+canExit = False
+while not canExit:
+    data = data_raw["all"][random.randrange(0, len(data_raw["all"]))]
+    if len(data.get("text")) > 8:
+        print("")
+        print("Cat fact of the day:")
+        print(data.get("text"))
+        print("")
+        print(f'Fact by: {data.get("user").get("name").get("first")} {data.get("user").get("name").get("last")}')
+        upvotes = int(data.get("upvotes"))
+        upvote_msg = "upvotes"
+        if upvotes == 1:
+            upvote_msg = "upvote"
+        print(f'This post has {upvotes} {upvote_msg}')
+        print("")
+        print("cat-fact.py written by Weeaboo420")
+        canExit = True
